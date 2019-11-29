10663372ea07154e56998edff06191fa08c51f52
Andrey Klochok <andreyklochok@gmail.com>
2019-11-29 13:15:15 +0200

    Fixed mobile layout overlay

--- src/components/ServerOrderWrapper.js
+++ src/components/ServerOrderWrapper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const ServerOrderWrapper = styled.div.attrs(props => ({className: 'use-bootstrap'}))`
   padding: 20px 0;
   background-color: ${props => props.configId !== null ? "white" : "white"};
-  height: 100%;
+  height: auto;
 `;
 
 export default ServerOrderWrapper;
